6fa4947c52094a101d11e6139e70f030c4a8eb27
johancano19 <104403251+johancano19@users.noreply.github.com>
2022-11-03 00:37:53 -0500

    se agregaron estilos a lista productos

--- farmacia/src/listaFarmacia/listaFarmacia.js
+++ farmacia/src/listaFarmacia/listaFarmacia.js
@@ -8,33 +8,32 @@ import datosProductoJson from "./productos.json"
 import Table from 'react-bootstrap/Table';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-const ListaFarmacia = () => 
-  {
-    const [datosProducto] = useState(datosProductoJson)
+const ListaFarmacia = () => {
+  const [datosProducto] = useState(datosProductoJson)
 
-    return (
-      <div>
+  return (
+    <div>
       <Row>
-          { 
-            datosProducto.map(
-              (producto)=>{
-                return(
+        {
+          datosProducto.map(
+            (producto) => {
+              return (
                 <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={producto.imgurl} />
-                <ListGroup className="list-group-flush">
-                  <ListGroup.Item><h2>{producto.nombre}</h2></ListGroup.Item>
-                  <ListGroup.Item><p>$ {producto.precio}</p></ListGroup.Item>
-                  <ListGroup.Item><p>stock: {producto.stock}</p></ListGroup.Item>
-                </ListGroup>
+                  <Card.Img variant="top" src={producto.imgurl} />
+                  <ListGroup className="list-group-flush">
+                    <ListGroup.Item><h2>{producto.nombre}</h2></ListGroup.Item>
+                    <ListGroup.Item><p>$ {producto.precio}</p></ListGroup.Item>
+                    <ListGroup.Item><p>stock: {producto.stock}</p></ListGroup.Item>
+                  </ListGroup>
                 </Card>
-                );
-              }
-            )
-          }
-        </Row>
-      </div>
-    )
-  }
-;
+              );
+            }
+          )
+        }
+      </Row>
+    </div>
+  )
+}
+  ;
 
 export default ListaFarmacia;
